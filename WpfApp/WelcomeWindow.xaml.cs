7d72779dc1e9c48cc7d04d4d7ce96e137909d1c0
Кирил Эктоф <kirill.ektoff@yandex.ru>
2022-01-24 19:38:46 +0700

    before install EF6 for MySql

---
+++ WpfApp/WelcomeWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace WpfApp
+{
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
